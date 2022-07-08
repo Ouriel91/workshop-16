da725c3fd8ce32ed79d76fb8f234da9f0af255f2
Ouriel91 <orieloh91@gmail.com>
2022-07-08 16:31:11 +0300

    finish workshop

--- cypress/integration/2-youtube-exercise/youtube-harry-potter-test.cy.js
+++ cypress/integration/2-youtube-exercise/youtube-harry-potter-test.cy.js
@@ -12,28 +12,40 @@ describe('open harry potter vs star wars movie', () => {
 
     it('page was loaded and can see youtube logo and at least one video in the list', () => {
         //Assert
-        //... your implementation
+        cy.get('.ytd-logo').should('be.visible') //logo
+        cy.get('.ytd-rich-grid-media').should('be.visible') //one video in the list
     })
 
     it('search for Harry Potter vs star wars movie with 33M views', () => {
         //Act
-        //... your implementation
+        cy.get('input#search').type('Harry Potter vs star wars')
+        cy.get('button#search-icon-legacy').click()
+        
         //Assert
-        //... your implementation
+        cy.get('.ytd-video-renderer').contains('Harry Potter VS Star Wars')
+        cy.get('.ytd-video-meta-block').contains('33M views')
     })
 
-    it('open video and check that it has author block and views are more than 33M', () => {
+    it('open video and check that it has author block and views are more than 33M', async() => {
         //Act
-        //... your implementation
-        //Assert
-        //... your implementation
-
+        cy.get('.ytd-video-renderer').first().click()
+        cy.get('.watch-active-metadata').should('be.visible') //author block
+        const viewsText = await new Promise((resolve) => {
+            cy.get('.view-count').invoke('text').then(value => resolve(value.toString()
+                .split(' ')[0]
+                .split(',')
+                .join('')))
+                //Assert
+                .should('be.gt', 33000000)
+        })
     })
     //
     it('BONUS: click share button and check that share url is https://youtu.be/9N5KyjM5v0c', () => {
         //Act
-        //... your implementation
+        cy.get('yt-formatted-string').contains('Share').click()
         //Assert
-        //... your implementation
+        cy.get('#share-url').should('be.visible')
+        cy.get('#share-url')
+            .should('have.value', 'https://youtu.be/9N5KyjM5v0c')
     })
 })
